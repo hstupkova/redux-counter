30df9067bfc8a90ff10472b2b603fc7fcf70ca6b
Hana Stupková <hstupkova@gmail.com>
2021-11-02 16:45:41 +0100

    migrating to redux toolkit (with 1 state slice)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 
+import { counterActions } from '../store';
+
 import classes from './Counter.module.css';
 
 const Counter = () => {
@@ -8,19 +10,19 @@ const Counter = () => {
   const showCounter = useSelector(state => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: 'increment' });
+    dispatch(counterActions.increment());
   };
 
   const increaseHandler = () => {
-    dispatch({ type: 'increase', amount: 5 });
+    dispatch(counterActions.increase(5)); // { type: SOME_ID, payload: 5}
   };
 
   const decrementHandler = () => {
-    dispatch({ type: 'decrement' });
+    dispatch(counterActions.decrement());
   };
 
   const toggleCounterHandler = () => {
-    dispatch({ type: 'toggle' });
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
